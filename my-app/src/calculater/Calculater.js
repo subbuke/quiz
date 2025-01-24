0cc8246758169d19894125d3f4e6830a7f5fd4e8
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-01-24 06:52:01 +0000

    calculater is adding

--- my-app/src/calculater/Calculater.js
+++ my-app/src/calculater/Calculater.js
@@ -3,8 +3,47 @@
 const Calculater = () => {
     return (
         <div>
-            <h1>Calculater</h1>
-            <h1>checking</h1>
+            
+            <form className="flex items-center max-w-sm mx-auto mt-4">
+                <label htmlFor="simple-search" className="sr-only">
+                    Search
+                </label>
+                <div className="relative w-full">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+
+                    </div>
+                    <input
+                        type="text"
+                        id="simple-search"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter first number"
+                        required=""
+                    />
+                </div>
+
+            </form>
+
+            <form className="flex items-center max-w-sm mx-auto mt-4">
+                <label htmlFor="simple-search" className="sr-only">
+                    Search
+                </label>
+                <div className="relative w-full">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+
+                    </div>
+                    <input
+                        type="text"
+                        id="simple-search"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter second number"
+                        required=""
+                    />
+                </div>
+
+            </form>
+
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">Default</button>
+
         </div>
     );
 };
